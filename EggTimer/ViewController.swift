1e37e6b352154d5d882a97b0a87036924e97dec9
NikhilPatra047 <patra.nikhil04@gmail.com>
2021-08-14 13:26:06 +0530

    EggTimer app
    A simple Timer app that goes off when timer hits the desired countdown time.

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,9 +7,70 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    @IBOutlet weak var titleLabel: UILabel!
     
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 10]
+    var secondsPassed = 0
+    var totalTime = 0
+    var timer = Timer()
+    
+    var player: AVAudioPlayer?
+
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
 
 
+            player?.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+      
+        
+        secondsPassed = 0
+        totalTime = 0
+        progressBar.progress = 0.0
+        
+        let hardness = sender.currentTitle!
+        
+        totalTime = eggTimes[hardness]!
+        
+        titleLabel.text = hardness
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            print(secondsPassed)
+            progressBar.progress = Float(secondsPassed) / Float(totalTime)
+        } else {
+            timer.invalidate()
+            playSound()
+            titleLabel.text = "Done!"
+            
+            let seconds = 4.0
+            DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+                self.titleLabel.text = "How do you like your eggs?"
+            }
+           
+        }
+    }
+    
 }
